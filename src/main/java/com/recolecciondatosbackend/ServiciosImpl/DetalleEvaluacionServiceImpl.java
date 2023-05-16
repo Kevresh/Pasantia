32d768858f5487abbcf9ec0fba4b910a595049a4
Kevin Gallego <kevin.gallego7480@gmail.com>
2023-05-15 21:00:38 -0500

    cambios

--- src/main/java/com/recolecciondatosbackend/ServiciosImpl/DetalleEvaluacionServiceImpl.java
+++ src/main/java/com/recolecciondatosbackend/ServiciosImpl/DetalleEvaluacionServiceImpl.java
@@ -34,12 +34,11 @@ public class DetalleEvaluacionServiceImpl implements DetalleEvaluacionService {
                 detalleEvaluacion.setObservaciones(detalleEvaluacionDTO.getObservaciones());
                 detalleEvaluacionRepository.save(detalleEvaluacion);
             }
-            return ResponseEntity.status(HttpStatus.CREATED).body("El formulario se ha creado correctamente");
+            return ResponseEntity.status(HttpStatus.CREATED).body("La evaluacion se ha creado correctamente");
         }catch (HttpMessageNotReadableException e) {
             // excepción al deserializar el JSON
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Error al procesar la solicitud: " + e.getMessage());
         } catch (Exception e) {
-            // otra excepción
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Ocurrió un error guardando el formulario");
         }
